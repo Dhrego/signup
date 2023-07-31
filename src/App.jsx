201b1e3118c3986270466a1ca62776725a6155b9
Andrea <andrea.dick3@gmail.com>
2023-07-31 21:07:51 +0200

    #

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,17 @@ import Nav from "./components/Nav";
 const App = () => {
   return (
     <>
-      <body className="container d-flex justify-content-center align-items-center">
-        
-          <Card>
-            <div className="col-md-6 py-4">
-              <Nav />
-              <Login />
-            </div>
+      <body className="container vh-100 d-flex justify-content-center align-items-center">
+        <Card>
+          <div className="col-md-6 py-4">
+            <Nav />
+            <Login />
+          </div>
 
-            <div className="col-md-6 d-flex justify-content-center align-items-center">
-              <img className="img-fluid" src="work.jpg" alt="" />
-            </div>
-          </Card>
-        
+          <div className="col-md-6 d-flex justify-content-center align-items-center">
+            <img className="img-fluid" src="work.jpg" alt="" />
+          </div>
+        </Card>
       </body>
     </>
   );
