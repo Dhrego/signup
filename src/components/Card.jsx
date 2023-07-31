31f11c090a30a0e334701524821573e1f92c85c4
Dhrego <andrea.dick3@gmail.com>
2023-07-31 20:56:34 +0200

    made changes to code to fit screen

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 const Card = (props) => {
-  return <div className="container col-10 my-5 row shadow rounded-5">{props.children}</div>;
+  return <div className="container my-5 row shadow rounded-5">{props.children}</div>;
 };
 export default Card;
  
